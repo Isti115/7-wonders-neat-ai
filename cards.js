d3fce01cf5abe5051c693d8c11286ac2a0ee86d8
Donkó István <istvan.donko@gmail.com>
2020-08-13 23:45:37 +0200

    Added MILITARY cards with Eszti

--- cards.js
+++ cards.js
@@ -52,7 +52,7 @@ export default [
     products: [ ResourceType.ORE, ResourceType.ORE ],
   })),
 
-  //
+  // Combined natural
   ...[6].map(playerCount => new Card({
     name: 'Tree Farm', playerCount, age: 1,
     type: CardType.NATURAL_GOODS,
@@ -90,22 +90,104 @@ export default [
     products: [ new Choice([ ResourceType.ORE, ResourceType.STONE ]) ],
   })),
 
-  //
-  ...[3, 4].map(playerCount => new Card({
-    name: 'Press', playerCount, age: 1,
-    type: CardType.MANUFACTURED_GOODS,
-    products: [ ResourceType.PAPYRUS ],
-  })),
-  ...[3, 4].map(playerCount => new Card({
-    name: 'Glassworks', playerCount, age: 1,
-    type: CardType.MANUFACTURED_GOODS,
-    products: [ ResourceType.GLASS ],
-  })),
-  ...[3, 4].map(playerCount => new Card({
+  // Manufactured
+  ...[3, 6].map(playerCount => new Card({
     name: 'Loom', playerCount, age: 1,
     type: CardType.MANUFACTURED_GOODS,
     products: [ ResourceType.LOOM ],
   })),
+  ...[3, 6].map(playerCount => new Card({
+    name: 'Glassworks', playerCount, age: 1,
+    type: CardType.MANUFACTURED_GOODS,
+    products: [ ResourceType.GLASS ],
+  })),
+  ...[3, 6].map(playerCount => new Card({
+    name: 'Press', playerCount, age: 1,
+    type: CardType.MANUFACTURED_GOODS,
+    products: [ ResourceType.PAPYRUS ],
+  })),
 
-  //
+  ...[3, 5].map(playerCount => new Card({
+    name: 'Loom', playerCount, age: 2,
+    type: CardType.MANUFACTURED_GOODS,
+    products: [ ResourceType.LOOM ],
+  })),
+  ...[3, 5].map(playerCount => new Card({
+    name: 'Glassworks', playerCount, age: 2,
+    type: CardType.MANUFACTURED_GOODS,
+    products: [ ResourceType.GLASS ],
+  })),
+  ...[3, 5].map(playerCount => new Card({
+    name: 'Press', playerCount, age: 2,
+    type: CardType.MANUFACTURED_GOODS,
+    products: [ ResourceType.PAPYRUS ],
+  })),
+
+  // Military
+  ...[3, 7].map(playerCount => new Card({
+    name: 'Stockade', playerCount, age: 1,
+    cost = [ ResourceType.WOOD ],
+    type: CardType.MILITARY,
+    shield: 1,
+  })),
+  ...[3, 5].map(playerCount => new Card({
+    name: 'Barracks', playerCount, age: 1,
+    cost = [ ResourceType.ORE ],
+    type: CardType.MILITARY,
+    shield: 1,
+  })),
+  ...[3, 4].map(playerCount => new Card({
+    name: 'Guard Tower', playerCount, age: 1,
+    cost = [ ResourceType.CLAY ],
+    type: CardType.MILITARY,
+    shield: 1,
+  })),
+  ...[3, 7].map(playerCount => new Card({
+    name: 'Walls', playerCount, age: 2,
+    cost = [ ResourceType.STONE, ResourceType.STONE, ResourceType.STONE ],
+    type: CardType.MILITARY,
+    shield: 2,
+  })),
+  ...[4, 6, 7].map(playerCount => new Card({
+    name: 'Training Ground', playerCount, age: 2,
+    cost = [ ResourceType.WOOD, ResourceType.ORE, ResourceType.ORE ],
+    type: CardType.MILITARY,
+    shield: 2,
+  })),
+  ...[3, 5].map(playerCount => new Card({
+    name: 'Stables', playerCount, age: 2,
+    cost = [ ResourceType.ORE, ResourceType.CLAY, ResourceType.WOOD ],
+    type: CardType.MILITARY,
+    shield: 2,
+  })),
+  ...[3, 6].map(playerCount => new Card({
+    name: 'Archery Range', playerCount, age: 2,
+    cost = [ ResourceType.WOOD, ResourceType.WOOD, ResourceType.ORE ],
+    type: CardType.MILITARY,
+    shield: 2,
+  })),
+  ...[3, 7].map(playerCount => new Card({
+    name: 'Fortifications', playerCount, age: 3,
+    cost = [ ResourceType.STONE, ResourceType.ORE, ResourceType.ORE, ResourceType.ORE ],
+    type: CardType.MILITARY,
+    shield: 3,
+  })),
+  ...[3, 5, 6].map(playerCount => new Card({
+    name: 'Circus', playerCount, age: 3,
+    cost = [ ResourceType.STONE, ResourceType.STONE, ResourceType.STONE, ResourceType.ORE ],
+    type: CardType.MILITARY,
+    shield: 3,
+  })),
+  ...[3, 4, 7].map(playerCount => new Card({
+    name: 'Arsenal', playerCount, age: 3,
+    cost = [ ResourceType.ORE, ResourceType.WOOD, ResourceType.WOOD, ResourceType.LOOM ],
+    type: CardType.MILITARY,
+    shield: 3,
+  })),
+  ...[3, 5].map(playerCount => new Card({
+    name: 'Siege Workshop', playerCount, age: 3,
+    cost = [ ResourceType.WOOD, ResourceType.CLAY, ResourceType.CLAY, ResourceType.CLAY ],
+    type: CardType.MILITARY,
+    shield: 3,
+  })),
 ]
